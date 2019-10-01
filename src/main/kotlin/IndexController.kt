43af2660cf5531b6c98ed16e8db055a96d802673
a21703957 <a21703957@alunos.ulht.pt>
2019-10-01 15:44:27 +0100

    problema

--- src/main/kotlin/IndexController.kt
+++ src/main/kotlin/IndexController.kt
@@ -1,11 +1,19 @@
 import org.springframework.stereotype.Controller
+import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 
 @Controller
 class IndexController {
 
-    @RequestMapping("/")
+    @RequestMapping("/tt", method = [RequestMethod.GET])
     fun index() : String{
-        return "index";
+        return "index2";
+    }
+
+    @RequestMapping("/perfil", method = [RequestMethod.GET])
+    fun perfil(model : ModelMap ) : String{
+        model.put("titulo", "A minha primeira pagina");
+        return "perfil";
     }
 }
