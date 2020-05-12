6e54ac92aa229c5cb7889a471fe81676603d7bb6
Pedro Alves <pedro.alves@ulusofona.pt>
2020-05-12 10:26:00 +0100

    Corrigido o problema do detalhe do badge

--- src/main/kotlin/pt/ulusofona/badges/controllers/TeacherController.kt
+++ src/main/kotlin/pt/ulusofona/badges/controllers/TeacherController.kt
@@ -1,9 +1,12 @@
 package pt.ulusofona.badges.controllers
 
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 import pt.ulusofona.badges.dao.Badge
 import pt.ulusofona.badges.dao.Student
 import pt.ulusofona.badges.forms.BadgeForm
@@ -53,7 +56,8 @@ class TeacherController(
 
     @GetMapping(value = ["/detailBadge/{badgeId}"])
     fun detailPage(@PathVariable badgeId: Long, model:ModelMap, request: HttpServletRequest): String{
-        val badge = badgeRepository.findById(badgeId)
+        val badge = badgeRepository.findByIdOrNull(badgeId) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
+
         model["badge"] = badge
         print("ESTE" + badge)
 
